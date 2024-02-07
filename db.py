6e7ccac40b7a62a315c644f1521903695b852fd7
yinyin13 <yinyin13@uw.edu>
2024-02-06 16:25:23 -0800

    Update db.py

--- db.py
+++ db.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-db_user = os.getenv('DB_USER')
-db_pw = os.getenv('DB_PASSWORD')
-db_host = os.getenv('DB_HOST')
-db_port = os.getenv('DB_PORT')
-db_name = os.getenv('DB_NAME')
+db_user = os.getenv('yinyin13')
+db_pw = os.getenv('matildaH1311')
+db_host = os.getenv('yinyin13-techin510-scraper.postgres.database.azure.com')
+db_port = os.getenv('5432')
+db_name = os.getenv('yinyin13')
 conn_str = f'postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}'
 
 def get_db_conn():
