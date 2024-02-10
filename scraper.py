2e6dcfcba95f1b25d251439b571c94d4f8450e6d
yinyin13 <yinyin13@uw.edu>
2024-02-09 16:15:15 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,27 +2,33 @@ import re
 import json
 import datetime
 from zoneinfo import ZoneInfo
-import html
-
 import requests
+from bs4 import BeautifulSoup
+import itertools
 
 from db import get_db_conn
 
 
-URL = 'https://visitseattle.org/events/page/'
+base_url = "https://visitseattle.org/events/page/"
+pages = 35
+events_ls = []
 URL_LIST_FILE = './data/links.json'
 URL_DETAIL_FILE = './data/data.json'
 
 def list_links():
-    res = requests.get(URL + '1/')
-    last_page_no = int(re.findall(r'bpn-last-page-link"><a href=".+?/page/(\d+?)/.+" title="Navigate to last page">', res.text)[0])
+    for pg in range (1, pages+1):
+        url = f'{base_url}{pg}'
+        res = requests.get(url)
 
-    links = []
-    for page_no in range(1, last_page_no + 1):
-        res = requests.get(URL + str(page_no) + '/')
-        links.extend(re.findall(r'<h3 class="event-title"><a href="(https://visitseattle.org/events/.+?/)" title=".+?">.+?</a></h3>', res.text))
+        soup = BeautifulSoup(res.text, "html.parser")
+        selector = "div.search-result-preview > div > h3 > a"
 
-    json.dump(links, open(URL_LIST_FILE, 'w'))
+        a_eles = soup.select(selector)
+
+        events_ls.append([x['href'] for x in a_eles])
+    
+    events_ls = list(itertools.chain.from_iterable(events_ls))
+    json.dump(events_ls, open(URL_LIST_FILE, 'w'))
 
 def get_detail_page():
     links = json.load(open(URL_LIST_FILE, 'r'))
@@ -30,14 +36,15 @@ def get_detail_page():
     for link in links:
         try:
             row = {}
-            res = requests.get(link)
-            row['title'] = html.unescape(re.findall(r'<h1 class="page-title" itemprop="headline">(.+?)</h1>', res.text)[0])
-            datetime_venue = re.findall(r'<h4><span>.*?(\d{1,2}/\d{1,2}/\d{4})</span> \| <span>(.+?)</span></h4>', res.text)[0]
+            res = requests.get(link).text
+            soup = BeautifulSoup(res, 'html.parser')
+            row['title'] = soup.find(class_="page-title").text
+            datetime_venue = soup.find('h4').text.split(" | ")
             row['date'] = datetime.datetime.strptime(datetime_venue[0], '%m/%d/%Y').replace(tzinfo=ZoneInfo('America/Los_Angeles')).isoformat()
             row['venue'] = datetime_venue[1].strip() # remove leading/trailing whitespaces
             metas = re.findall(r'<a href=".+?" class="button big medium black category">(.+?)</a>', res.text)
-            row['category'] = html.unescape(metas[0])
-            row['location'] = metas[1]
+            row['category'] = soup.find(class_="button big medium black category").text
+            row['location'] = soup.select_one('a.button.big.medium.black.category[href*=event_regions]').text
             data.append(row)
         except IndexError as e:
             print(f'Error: {e}')
