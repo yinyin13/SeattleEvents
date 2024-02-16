192536b2d779d5b684f252992c8adacaffb61cac
yinyin13 <yinyin13@uw.edu>
2024-02-16 00:42:55 -0800

    Update db.py

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ db_pw = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
 db_name = os.getenv('DB_NAME')
-conn_str = f'postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}'
+conn_str = f'postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}?sslmode=require'
 
 def get_db_conn():
     conn = psycopg2.connect(conn_str)
